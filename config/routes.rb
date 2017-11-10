731142f0067c03c3f5b748256763042e87d96109
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2017-11-10 19:43:32 +0900

    ユーザ名追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
+  resources :contacts
   get 'favorites/create'
 
   get 'favorites/destroy'
 
   get 'sessions/new'
 
-  root to: "blogs#index"
-  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'blogs#index'
+  resources :sessions, only: %i[new create destroy]
   resources :users
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: %i[create destroy]
   # resources :blogs
   resources :blogs do
     collection do
       get :confirm
     end
   end
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107065609) do
+ActiveRecord::Schema.define(version: 20171110093651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20171107065609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "favorites", force: :cascade do |t|
