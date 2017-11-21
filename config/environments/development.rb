6a98cc4b2bce6ea7eed1368bf83ca3940757b830
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2017-11-21 14:55:15 +0900

    productionの環境変数修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,17 +45,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings =
-    {
-      user_name: ENV['SENDGRID_USERNAME'],
-      password: ENV['SENDGRID_PASSWORD'],
-      domain: 'example.com',
-      address: 'smtp.SendGrid.net',
-      port: 587,
-      authentication: :plain,
-      enable_starttls_auto: true
-    }
+
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
