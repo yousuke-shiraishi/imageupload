f54da29aaef7686f2d35bc2eacd8173450101798
yousuke-shiraishi <y-shiraishi@nexyzbb.ne.jp>
2017-11-27 17:31:44 +0900

    画像アップロード課題

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -91,7 +91,7 @@ class BlogsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def blog_params
-    params.require(:blog).permit(:content)
+    params.require(:blog).permit(:title,:content,:image)
   end
 
   def login_validate
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124070440) do
+ActiveRecord::Schema.define(version: 20171127063621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20171124070440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "title"
+    t.string "image"
   end
 
   create_table "contacts", force: :cascade do |t|
